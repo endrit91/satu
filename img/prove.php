f4cc4815e8d80f913ca904bfae89281b778af55e
endrit91 <endrit91@users.noreply.github.com>
2016-11-25 12:52:56 +0100

    Adding media

---
+++ img/prove.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+
+</style>
+  </head>
+<body>
+  <div style="background-image: url('/C:/xampp/htdocs/Projekti_im/bla.jpg');height:200px;width:1200px;">
+  </div>
+</body>
+</html>
